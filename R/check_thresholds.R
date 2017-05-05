cb8d1fed360ad925c5e21e0f5a8de41fe5842f46
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2017-05-05 14:49:24 +0200

    check_thresholds() function for preparing data for report table

--- R/check_thresholds.R
+++ R/check_thresholds.R
@@ -11,9 +11,34 @@
 check_thresholds <- function(df, #haridwar_day_list,
                              thresholds = aquanes.report::get_thresholds()) {
 
-thresholds$ParameterThresholdComparison <- gsub(pattern = "^[=]",
+thresholds$ParameterThresholdComparisonR <- gsub(pattern = "^[=]",
                                                 replacement = "==",
                                                 thresholds$ParameterThresholdComparison)
+
+
+
+thresholds$numberOfExceedance <- 0
+thresholds$exceedanceLabel <- "Satisfies threshold"
+
+
+for (idx in seq_len(nrow(thresholds))) {
+  
+  cond1 <- df$ParameterCode == thresholds$ParameterCode[idx] & df$SiteCode == thresholds$SiteCode[idx]
+  cond2 <-  eval(parse(text=sprintf("df$ParameterValue %s %s", 
+                                    thresholds$ParameterThresholdComparisonR[idx],
+                                    thresholds$ParameterThreshold[idx])))
+  
+  condition <- cond1 & cond2
+  
+  number_of_exceedances <- nrow(df[condition,])
+  
+  if(number_of_exceedances > 0) {
+    thresholds$numberOfExceedance[idx] <- number_of_exceedances
+    thresholds$exceedanceLabel[idx] <- "Not satisfying threshold"
+  }
+}
+
+return(thresholds)
 
 
 }
